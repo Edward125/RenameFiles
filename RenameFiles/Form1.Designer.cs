f87176d342f2d9f28a3faf0142a5d8d932bc7717
Edward Song <edward_song@yeah.net>
2018-10-22 21:20:57 +0800

    v1.0.1.0

---
+++ RenameFiles/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace RenameFiles
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(357, 25);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(76, 30);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(86, 31);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(265, 21);
+            this.txtFolder.TabIndex = 1;
+            this.txtFolder.DoubleClick += new System.EventHandler(this.txtFolder_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "文件夹地址";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtFolder);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnGo);
+            this.groupBox1.Location = new System.Drawing.Point(12, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(451, 78);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 127);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ RenameFiles/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Edward;
+using System.IO;
+
+namespace RenameFiles
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void frmMain_Load(object sender, EventArgs e)
+            
+        {
+            this.Text = "批量删除文件名中的-,Ver:" + Application.ProductVersion;
+            txtFolder.SetWatermark("双击此处选择需要批量修改文件名的文件夹");
+        }
+
+
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFolder.Text.Trim())) 
+            {
+                txtFolder.Focus ();
+                return;
+            }
+
+
+            if (!Directory.Exists (txtFolder.Text.Trim ()))
+            {
+                MessageBox.Show("文件夹地址不存在,请重新输入", "文件夹不存在", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                txtFolder.SelectAll();
+                txtFolder.Focus();
+               return;
+            }
+
+            txtFolder.Enabled = false;
+            btnGo.Enabled = false;
+
+            long  i = 0;
+
+            //DirectoryInfo di = new DirectoryInfo(txtFolder.Text.Trim ());
+            //FileInfo[] fis = di.GetFiles();
+            //foreach (FileInfo fi in fis)
+            //{
+            //    if (fi.Name.Contains("-"))
+            //    {
+            //        string newName = fi.Name.Replace("-", "");
+            //        File.Move(fi.FullName, txtFolder.Text.Trim() + @"\" + newName);
+            //        File.Delete(fi.FullName);
+            //        i++;
+            //    }
+
+            //}
+
+            List<FileInformation> list = new List<FileInformation>();
+            list =  DirectoryAllFiles.GetAllFiles(new System.IO.DirectoryInfo(@txtFolder.Text.Trim()));
+
+            foreach (var item in list)
+            {
+                //
+                //Console.WriteLine(string.Format("文件名：{0}---文件目录{1}", item.FileName, item.FilePath));
+                if (item.FileName.Contains("-"))
+                {
+                    //this.Text = "正在处理:" + item.FileName;
+                    string newName = item.FileName.Replace("-", "");
+                    try
+                    {
+
+                        if (File.Exists(item.FilePath))
+                        {
+                            File.Move(item.FilePath, item.FileDirectory + @"\" + newName);
+                            File.Delete(item.FilePath);
+                            i++;
+                        }
+
+
+             
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("修改文件名出现错误," + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        
+                    }
+     
+
+                }
+            }
+
+
+
+            MessageBox.Show("共计完成修改文件个数:" + i, "Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            txtFolder.Enabled = true;
+            btnGo.Enabled = true;
+
+
+        }
+
+        private void txtFolder_DoubleClick(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+                txtFolder.Text = fbd.SelectedPath;
+        }
+
+
+
+        public class DirectoryAllFiles
+        {
+            static List<FileInformation> FileList = new List<FileInformation>();
+            public static List<FileInformation> GetAllFiles(DirectoryInfo dir)
+            {
+                FileInfo[] allFile = dir.GetFiles();
+                foreach (FileInfo fi in allFile)
+                {
+                    FileList.Add(new FileInformation { FileName = fi.Name, FilePath = fi.FullName , FileDirectory  = fi.DirectoryName });
+                }
+                DirectoryInfo[] allDir = dir.GetDirectories();
+                foreach (DirectoryInfo d in allDir)
+                {
+                    GetAllFiles(d);
+                }
+                return FileList;
+            }
+        }
+
+        public class FileInformation
+        {
+            public string FileName { get; set; }
+            public string FilePath { get; set; }
+            public string FileDirectory { get; set; }
+        }
+
+
+    }
+}
